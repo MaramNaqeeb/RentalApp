6c0c3ba66afab27d374e1475f956bb9e37814a69
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-07-21 15:22:17 +0300

    rentalApp

---
+++ RentalApp/src/main/java/renting/Apartment.java
@@ -0,0 +1,8 @@
+package renting;
+
+public class Apartment extends Building {
+
+	public Apartment(String address, String name, double size, double price, int year) {
+		super(address, name, size, price, year);
+	}
+}
---
+++ RentalApp/src/main/java/renting/Shop.java
@@ -0,0 +1,8 @@
+package renting;
+
+class Shop extends Building {
+	public Shop(String address, String name, double size, double price, int year) {
+		super(address, name, size, price, year);
+	}
+
+}
